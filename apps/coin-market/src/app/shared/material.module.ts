8a78dc945317bbb47b16618ae599d90e566bbb56
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2021-03-26 16:24:49 +0300

    implemented a new module for angular material

--- apps/coin-market/src/app/shared/material.module.ts
+++ apps/coin-market/src/app/shared/material.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from "@angular/core";
 import { MatSliderModule } from "@angular/material/slider";
 import { MatButtonModule } from "@angular/material/button";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { LayoutModule } from "@angular/cdk/layout";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
 
-const modules: any[] = [MatSliderModule, MatButtonModule];
+import { MatCheckboxModule } from "@angular/material/checkbox";
+
+const modules: any[] = [
+  MatSliderModule,
+  MatButtonModule,
+  MatGridListModule,
+  LayoutModule,
+  MatCardModule,
+  MatIconModule,
+  MatCheckboxModule,
+  MatProgressBarModule,
+];
 
 @NgModule({
   imports: [...modules],
