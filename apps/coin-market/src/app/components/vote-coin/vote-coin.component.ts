3a1212a7a7cb519c91b736f1a4f82485ad020314
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2021-04-02 13:59:18 +0300

    arranged component to work with dynamic data

--- apps/coin-market/src/app/components/vote-coin/vote-coin.component.ts
+++ apps/coin-market/src/app/components/vote-coin/vote-coin.component.ts
@@ -1,20 +1,67 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Observable, Subscription } from "rxjs";
+import { Coin } from "../../models/coin";
+import { VoteCoin } from "../../models/voteCoin.model";
+import { VoteCoinData } from "../../models/voteCoinData.model";
+import { CoinService } from "../../shared/services/coin.service";
+import { LocalStorageService } from "../../shared/services/local-storage.service";
 
 @Component({
   selector: "vote-coin",
   templateUrl: "./vote-coin.component.html",
   styleUrls: ["./vote-coin.component.scss"],
 })
-export class VoteCoinComponent {
+export class VoteCoinComponent implements OnInit, OnDestroy {
+  coinSubscription: Subscription;
+  coinName: string;
+  votes$: Observable<VoteCoinData>;
+
   didVote = false;
   voted: number;
-  good = 72;
+
+  constructor(
+    private localStorageService: LocalStorageService,
+    private coinService: CoinService
+  ) {}
+
+  ngOnInit(): void {
+    this.coinSubscription = this.coinService
+      .getCoin()
+      .subscribe((x) => (this.coinName = x.name));
+
+    this.votes$ = this.coinService.getVotes();
+
+    this.checkVote();
+  }
+
+  checkVote() {
+    const vote = this.localStorageService.getItem(`${this.coinName}_vote`);
+
+    if (vote) {
+      const voteObj: VoteCoin = JSON.parse(vote);
+
+      if (voteObj.expireDate < new Date().getMilliseconds())
+        this.localStorageService.removeItem(`${this.coinName}_vote`);
+      else {
+        this.didVote = true;
+        this.voted = voteObj.value;
+      }
+    }
+  }
 
   vote(value: number) {
     this.didVote = true;
     this.voted = value;
 
-    if (value < 0) this.good--;
-    else this.good++;
+    this.localStorageService.setItem(
+      `${this.coinName}_vote`,
+      JSON.stringify(new VoteCoin(value, new Date().setHours(24)))
+    );
+
+    //todo post vote
+  }
+
+  ngOnDestroy(): void {
+    this.coinSubscription.unsubscribe();
   }
 }
