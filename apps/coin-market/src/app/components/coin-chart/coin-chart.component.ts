184afbeccf2b0fd5d210685c694164fdcb2134cf
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2021-04-05 14:03:02 +0300

    renamed coin.service to overview.service

--- apps/coin-market/src/app/components/coin-chart/coin-chart.component.ts
+++ apps/coin-market/src/app/components/coin-chart/coin-chart.component.ts
@@ -1,36 +1,38 @@
-import { CurrencyPipe } from "@angular/common";
-import { Component } from "@angular/core";
-import * as Highcharts from "highcharts/highstock";
+import { CurrencyPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts/highstock';
 
-import IndicatorsCore from "highcharts/indicators/indicators";
-import { Observable } from "rxjs";
-import { ChartData } from "../../models/chart-data";
-import { CoinService } from "../../shared/services/coin.service";
+import IndicatorsCore from 'highcharts/indicators/indicators';
+import { Observable } from 'rxjs';
+import { ChartData } from '../../models/chart-data';
+import { OverviewService } from '../../shared/services/overview.service';
 IndicatorsCore(Highcharts);
 
 @Component({
-  selector: "coin-chart",
-  styleUrls: ["./coin-chart.component.scss"],
-  templateUrl: "./coin-chart.component.html",
+    selector: 'coin-chart',
+    styleUrls: ['./coin-chart.component.scss'],
+    templateUrl: './coin-chart.component.html',
 })
 export class CoinChartComponent {
-  Chart: Highcharts.Chart;
-  chartCallback: Highcharts.ChartCallbackFunction = (
-    chart: Highcharts.Chart
-  ) => {
-    this.Chart = chart;
-  };
-  
+    Chart: Highcharts.Chart;
+    chartCallback: Highcharts.ChartCallbackFunction = (
+        chart: Highcharts.Chart
+    ) => {
+        this.Chart = chart;
+    };
 
-  //data
-  selected = "Price";
+    //data
+    selected = 'Price';
 
-  //legend
-  legend = ["USD", "BTC"];
+    //legend
+    legend = ['USD', 'BTC'];
 
-  chartData$: Observable<ChartData>;
+    chartData$: Observable<ChartData>;
 
-  constructor(private currencyPipe: CurrencyPipe, private coinService: CoinService) {
-    this.chartData$ = this.coinService.chartData$;
-  }
+    constructor(
+        private currencyPipe: CurrencyPipe,
+        private overviewService: OverviewService
+    ) {
+        this.chartData$ = this.overviewService.chartData$;
+    }
 }
--- apps/coin-market/src/app/components/vote-coin/vote-coin.component.ts
+++ apps/coin-market/src/app/components/vote-coin/vote-coin.component.ts
@@ -1,67 +1,67 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { Observable, Subscription } from "rxjs";
-import { Coin } from "../../models/coin";
-import { VoteCoin } from "../../models/voteCoin.model";
-import { VoteCoinData } from "../../models/voteCoinData.model";
-import { CoinService } from "../../shared/services/coin.service";
-import { LocalStorageService } from "../../shared/services/local-storage.service";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Coin } from '../../models/coin';
+import { VoteCoin } from '../../models/voteCoin.model';
+import { VoteCoinData } from '../../models/voteCoinData.model';
+import { OverviewService } from '../../shared/services/overview.service';
+import { LocalStorageService } from '../../shared/services/local-storage.service';
 
 @Component({
-  selector: "vote-coin",
-  templateUrl: "./vote-coin.component.html",
-  styleUrls: ["./vote-coin.component.scss"],
+    selector: 'vote-coin',
+    templateUrl: './vote-coin.component.html',
+    styleUrls: ['./vote-coin.component.scss'],
 })
 export class VoteCoinComponent implements OnInit, OnDestroy {
-  coinSubscription: Subscription;
-  coinName: string;
-  votes$: Observable<VoteCoinData>;
+    coinSubscription: Subscription;
+    coinName: string;
+    votes$: Observable<VoteCoinData>;
 
-  didVote = false;
-  voted: number;
+    didVote = false;
+    voted: number;
 
-  constructor(
-    private localStorageService: LocalStorageService,
-    private coinService: CoinService
-  ) {}
+    constructor(
+        private localStorageService: LocalStorageService,
+        private overviewService: OverviewService
+    ) {}
 
-  ngOnInit(): void {
-    // this.coinSubscription = this.coinService
-    //   .getCoin()
-    //   .subscribe((x) => (this.coinName = x.name));
+    ngOnInit(): void {
+        // this.coinSubscription = this.coinService
+        //   .getCoin()
+        //   .subscribe((x) => (this.coinName = x.name));
 
-    this.votes$ = this.coinService.getVotes();
+        this.votes$ = this.overviewService.coinVotes$;
 
-    this.checkVote();
-  }
-
-  checkVote() {
-    const vote = this.localStorageService.getItem(`${this.coinName}_vote`);
-
-    if (vote) {
-      const voteObj: VoteCoin = JSON.parse(vote);
-
-      if (voteObj.expireDate < new Date().getMilliseconds())
-        this.localStorageService.removeItem(`${this.coinName}_vote`);
-      else {
-        this.didVote = true;
-        this.voted = voteObj.value;
-      }
+        this.checkVote();
     }
-  }
 
-  vote(value: number) {
-    this.didVote = true;
-    this.voted = value;
+    checkVote() {
+        const vote = this.localStorageService.getItem(`${this.coinName}_vote`);
 
-    this.localStorageService.setItem(
-      `${this.coinName}_vote`,
-      JSON.stringify(new VoteCoin(value, new Date().setHours(24)))
-    );
+        if (vote) {
+            const voteObj: VoteCoin = JSON.parse(vote);
 
-    //todo post vote
-  }
+            if (voteObj.expireDate < new Date().getMilliseconds())
+                this.localStorageService.removeItem(`${this.coinName}_vote`);
+            else {
+                this.didVote = true;
+                this.voted = voteObj.value;
+            }
+        }
+    }
 
-  ngOnDestroy(): void {
-    this.coinSubscription.unsubscribe();
-  }
+    vote(value: number) {
+        this.didVote = true;
+        this.voted = value;
+
+        this.localStorageService.setItem(
+            `${this.coinName}_vote`,
+            JSON.stringify(new VoteCoin(value, new Date().setHours(24)))
+        );
+
+        //todo post vote
+    }
+
+    ngOnDestroy(): void {
+        this.coinSubscription.unsubscribe();
+    }
 }
