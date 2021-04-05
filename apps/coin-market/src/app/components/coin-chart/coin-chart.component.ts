78918eaec65c97e1fc592f140c81866ce8dd332a
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2021-04-06 00:54:27 +0300

    optimized line chart by replacing static fields with dynamic fields

--- apps/coin-market/src/app/components/coin-chart/coin-chart.component.ts
+++ apps/coin-market/src/app/components/coin-chart/coin-chart.component.ts
@@ -1,9 +1,9 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import * as Highcharts from 'highcharts/highstock';
 
 import IndicatorsCore from 'highcharts/indicators/indicators';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { ChartData } from '../../models/chart-data';
 import { OverviewService } from '../../shared/services/overview.service';
 IndicatorsCore(Highcharts);
@@ -13,13 +13,14 @@ IndicatorsCore(Highcharts);
     styleUrls: ['./coin-chart.component.scss'],
     templateUrl: './coin-chart.component.html',
 })
-export class CoinChartComponent {
+export class CoinChartComponent implements OnInit, OnDestroy {
     chart: Highcharts.Chart;
     chartCallback: Highcharts.ChartCallbackFunction = (
         chart: Highcharts.Chart
     ) => {
         this.chart = chart;
     };
+    series: Highcharts.SeriesOptionsType[] = [];
 
     //data
     selected = 'Price';
@@ -27,12 +28,101 @@ export class CoinChartComponent {
     //legend
     legend = ['USD', 'BTC'];
 
-    chartData$: Observable<ChartData>;
+    chartDataSubscription: Subscription;
+
+    generateTooltipTemplate(
+        val: number,
+        headerColor: string = 'rgb(207, 214, 228)',
+        isCoin: boolean = false,
+        headerTitle: string = 'Price'
+    ) {
+        return `
+        <div class="highcharts-tooltip__line">
+          <div class="highcharts-tooltip__line__header">
+            <div class="highcharts-tooltip__line__header__color" style="background:${headerColor}">
+            </div>
+            ${headerTitle}${isCoin ? `(BTC)` : ''}: 
+          </div>
+          <div class="highcharts-tooltip__line__body">
+          ${
+              isCoin
+                  ? this.currencyPipe.transform(val, 'BTC', '', '1.2-6') +
+                    ' BTC'
+                  : this.currencyPipe.transform(val, 'USD', 'symbol', '1.0-6')
+          }
+          </div>
+        </div>`;
+    }
 
     constructor(
         private currencyPipe: CurrencyPipe,
         private overviewService: OverviewService
-    ) {
-        this.chartData$ = this.overviewService.chartData$;
+    ) {}
+
+    ngOnInit(): void {
+        const _this = this; //points to class
+        const commonSeriesOptions = {
+            states: {
+                hover: { lineWidth: 2 },
+            },
+            label: { enabled: false },
+        };
+        this.chartDataSubscription = this.overviewService.chartData$.subscribe(
+            (data) => {
+                this.series.push({
+                    id: 'usd',
+                    name: 'USD',
+                    color: 'rgb(22, 199, 132)',
+                    type: 'line',
+                    visible: true,
+                    data: [...data.usd],
+                    tooltip: {
+                        pointFormatter: function () {
+                            return _this.generateTooltipTemplate(
+                                this.y,
+                                this.color as string
+                            );
+                        },
+                    },
+                    ...commonSeriesOptions,
+                });
+                this.series.push({
+                    id: 'BTC',
+                    name: 'BTC',
+                    color: 'rgb(255, 187, 31)',
+                    type: 'line',
+                    visible: false,
+                    data: [...data.coin],
+                    tooltip: {
+                        pointFormatter: function () {
+                            return _this.generateTooltipTemplate(
+                                this.y,
+                                this.color as string,
+                                true
+                            );
+                        },
+                    },
+                    ...commonSeriesOptions,
+                });
+                this.series.push({
+                    id: 'vol',
+                    name: 'Vol 24h',
+                    type: 'column',
+                    visible: false,
+                    data: [...data.volume],
+                    tooltip: {
+                        pointFormatter: function () {
+                            const value = this.y;
+                            return _this.generateTooltipTemplate(value);
+                        },
+                    },
+                    ...commonSeriesOptions,
+                });
+            }
+        );
+    }
+
+    ngOnDestroy(): void {
+        this.chartDataSubscription.unsubscribe();
     }
 }
--- apps/coin-market/src/app/shared/components/line-chart/line-chart.component.ts
+++ apps/coin-market/src/app/shared/components/line-chart/line-chart.component.ts
@@ -1,117 +1,41 @@
-import { CurrencyPipe } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
-
-import * as Highcharts from "highcharts/highstock";
-
-import IndicatorsCore from "highcharts/indicators/indicators";
-import { ChartData } from '../../../models/chart-data';
+import * as Highcharts from 'highcharts/highstock';
+import IndicatorsCore from 'highcharts/indicators/indicators';
+import HC_exporting from 'highcharts/modules/exporting';
+HC_exporting(Highcharts);
 IndicatorsCore(Highcharts);
 
 @Component({
-  selector: 'coin-market-line-chart',
-  templateUrl: './line-chart.component.html',
-  styleUrls: ['./line-chart.component.scss']
+    selector: 'coin-market-line-chart',
+    templateUrl: './line-chart.component.html',
+    styleUrls: ['./line-chart.component.scss'],
 })
 export class LineChartComponent implements OnInit {
+    highcharts: typeof Highcharts;
+    @Input() constructorType: string;
+    @Input() callback: Highcharts.ChartCallbackFunction;
+    @Input() series: Highcharts.SeriesOptionsType[];
 
-  highcharts: typeof Highcharts;
-  @Input() constructorType: string;
-  @Input() chartData: ChartData;
-  @Input() callback;
+    chartOptions: Highcharts.Options;
 
-  Chart: Highcharts.Chart;
-  chartCallback: Highcharts.ChartCallbackFunction = (
-    chart: Highcharts.Chart
-  ) => {
-    this.Chart = chart;
-  };
-  chartOptions: Highcharts.Options;
+    ngOnInit() {
+        this.highcharts = Highcharts;
 
-  //data
-  selected = "Price";
-
-  //legend
-  legend = ["USD", "BTC"];
-
-
-  constructor(public currencyPipe: CurrencyPipe) {
-  }
-
-  ngOnInit() {
-
-    
-    this.highcharts = Highcharts;
-
-    this.chartOptions = {
-      rangeSelector: {
-        inputEnabled: false,
-        enabled: false,
-      },
-      tooltip: {
-        split: false,
-        shared: true,
-        useHTML: true,
-        headerFormat: `<div class="highcharts-tooltip__line">
+        this.chartOptions = {
+            rangeSelector: {
+                inputEnabled: false,
+                enabled: false,
+            },
+            tooltip: {
+                split: false,
+                shared: true,
+                useHTML: true,
+                headerFormat: `<div class="highcharts-tooltip__line">
         <div class="highcharts-tooltip__line__body">{point.key}
         </div>
       </div>`,
-      },
-      series: [
-        {
-          id: "usd",
-          type: "line",
-          color: "var(--theme-color)",
-          data: [...this.chartData.usd],
-          name: "USD",
-          tooltip: {
-            pointFormatter: function () {
-              return `
-                      <div class="highcharts-tooltip__line">
-                        <div class="highcharts-tooltip__line__header">
-                          <div class="highcharts-tooltip__line__header__color" style="background:${
-                            this.color
-                          }">
-                          </div>
-                          Price:
-                        </div>
-                        <div class="highcharts-tooltip__line__body">
-                         ${this.y}
-                        </div>
-                      </div>`;
             },
-          },
-          states: {
-            hover: { lineWidth: 2 },
-          },
-          label: { enabled: false },
-        },
-        {
-          id: "coin",
-          visible: false,
-          type: "line",
-          color: "rgb(255, 187, 31)",
-          data: [...this.chartData.coin],
-          name: "BTC",
-          tooltip: {
-            pointFormatter: function () {
-              return `<div class="highcharts-tooltip__line">
-            <div class="highcharts-tooltip__line__header">
-              <div class="highcharts-tooltip__line__header__color" style="background:${this.color}">
-              </div>
-              Price(${this.series.name}):
-            </div>
-            <div class="highcharts-tooltip__line__body">
-             ${this.y} ${this.series.name}
-            </div>
-          </div>`;
-            },
-          },
-          states: {
-            hover: { lineWidth: 2 },
-          },
-          label: { enabled: false },
-        },
-      ],
-    };
-  }
+            series: [...this.series],
+        };
+    }
 }
