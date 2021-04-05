fb9019381300e4b3150291472fa4763780cc96e0
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2021-04-06 00:52:30 +0300

    implemented model for chartData

--- apps/coin-market/src/app/models/chart-data.ts
+++ apps/coin-market/src/app/models/chart-data.ts
@@ -1,5 +1,11 @@
 export class ChartData {
     usd: any[];
     coin: any[];
-}
+    volume: any[];
 
+    constructor(usd?, coin?, volume?) {
+        this.usd = usd || [];
+        this.coin = coin || [];
+        this.volume = volume || [];
+    }
+}
