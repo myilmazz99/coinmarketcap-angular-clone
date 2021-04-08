6184cb68e09c64d34b07f1a5b4899759aba33a19
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2021-04-08 23:25:34 +0300

    extended chart data

--- apps/coin-market/src/app/models/chart-data.ts
+++ apps/coin-market/src/app/models/chart-data.ts
@@ -2,10 +2,12 @@ export class ChartData {
     usd: any[];
     coin: any[];
     volume: any[];
+    marketCap: { usd: any[]; coin: any[] };
 
-    constructor(usd?, coin?, volume?) {
+    constructor(usd?, coin?, volume?, marketCap?) {
         this.usd = usd || [];
         this.coin = coin || [];
         this.volume = volume || [];
+        this.marketCap = marketCap || { usd: [], coin: [] };
     }
 }
