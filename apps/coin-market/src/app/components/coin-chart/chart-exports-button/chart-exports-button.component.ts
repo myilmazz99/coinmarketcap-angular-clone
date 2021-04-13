d2f57ae01263b0e706a25b6fa48a10e3fd0c088d
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2021-04-14 00:26:53 +0300

    implemented support for .xlsx export

--- apps/coin-market/src/app/components/coin-chart/chart-exports-button/chart-exports-button.component.ts
+++ apps/coin-market/src/app/components/coin-chart/chart-exports-button/chart-exports-button.component.ts
@@ -1,12 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ChartData } from '../../../models/chart-data';
+import * as exceljs from 'exceljs';
 
 @Component({
     selector: 'chart-exports-button',
     templateUrl: './chart-exports-button.component.html',
     styleUrls: ['./chart-exports-button.component.scss'],
 })
-export class ChartExportsButtonComponent implements OnInit {
+export class ChartExportsButtonComponent {
     @Input() chart: Highcharts.Chart;
+    @Input() chartData: ChartData;
+    @Input() selectedTab: string;
     exportOptions = [
         { text: 'Download PNG Image', type: 'image/png' },
         { text: 'Download JPEG Image', type: 'image/jpeg' },
@@ -24,5 +28,65 @@ export class ChartExportsButtonComponent implements OnInit {
         );
     }
 
-    ngOnInit(): void {}
+    handleXLSX() {
+        const wb = new exceljs.Workbook();
+        const sheet = wb.addWorksheet('Chart');
+        const currencyFormat = '"$"#,##0.00;[Red]-"$"#,##0.00';
+        const { min, max } = this.chart.xAxis[0].getExtremes();
+
+        sheet.columns = [
+            {
+                header: 'Date',
+                key: 'date',
+                width: 35,
+                style: { numFmt: 'dd/mm/yyyy HH:MM:SS' },
+            },
+            {
+                header: 'USD',
+                key: 'usd',
+                width: 20,
+                style: { numFmt: currencyFormat },
+            },
+            { header: 'BTC', key: 'btc', width: 20 },
+            {
+                header: 'Volume',
+                key: 'volume',
+                width: 20,
+                style: { numFmt: currencyFormat },
+            },
+        ];
+
+        const data: Array<{
+            date: Date;
+            usd: number;
+            btc: number;
+            volume: number;
+        }> = [];
+
+        for (let i = 0; i < this.chartData.volume.length; i++) {
+            const date: number = this.chartData.volume[i][0];
+            if (date <= max && date >= min)
+                data.push({
+                    date: new Date(date),
+                    usd: this.chartData[this.selectedTab || 'price'].usd[i][1],
+                    btc: this.chartData[this.selectedTab || 'price'].coin[i][1],
+                    volume: this.chartData.volume[i][1],
+                });
+        }
+
+        sheet.addRows(data);
+
+        wb.xlsx.writeBuffer().then(function (data: exceljs.Buffer) {
+            const blob = new Blob([data], {
+                type:
+                    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            });
+            const url = window.URL.createObjectURL(blob);
+            const anchor = document.createElement('a');
+            anchor.href = url;
+            anchor.download = 'chart.xlsx';
+            anchor.click();
+            window.URL.revokeObjectURL(url);
+        });
+    }
 }
