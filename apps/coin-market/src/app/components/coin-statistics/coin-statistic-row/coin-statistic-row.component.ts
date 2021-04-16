68a8dea9746af0aadc49ddfeb98f86a82bad7255
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2021-04-16 15:21:42 +0300

    refactored to look cleaner

--- apps/coin-market/src/app/components/coin-statistics/coin-statistic-row/coin-statistic-row.component.ts
+++ apps/coin-market/src/app/components/coin-statistics/coin-statistic-row/coin-statistic-row.component.ts
@@ -1,17 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'coin-statistic-row',
     templateUrl: './coin-statistic-row.component.html',
     styleUrls: ['./coin-statistic-row.component.scss'],
 })
-export class CoinStatisticRowComponent {
+export class CoinStatisticRowComponent implements OnInit {
     @Input() title: string;
     @Input() value: number | number[] | string;
     @Input() isCurrency: boolean;
     @Input() dayTag: boolean;
     @Input() percentage: number;
     @Input() tooltip: string;
+    comparison: Comparison;
+
+    ngOnInit(): void {
+        if (this.percentage) this.comparison = new Comparison(this.percentage);
+    }
 
     isArray() {
         return this.value instanceof Array;
@@ -21,11 +26,21 @@ export class CoinStatisticRowComponent {
         return typeof this.value === 'string';
     }
 
-    handleComparisonClass(val: number) {
+    handleComparisonClass() {
         return {
-            'comparison--up': val > 0,
-            'comparison--down': val < 0,
+            'comparison--up': this.percentage > 0,
+            'comparison--down': this.percentage < 0,
             'comparison--small': this.value,
         };
+    }
+}
+
+class Comparison {
+    icon?: 'expand_more' | 'expand_less';
+    percentage?: number;
+
+    constructor(percentage: number) {
+        this.icon = percentage < 0 ? 'expand_more' : 'expand_less';
+        this.percentage = percentage < 0 ? percentage * -1 : percentage;
     }
 }
