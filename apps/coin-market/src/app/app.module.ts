caa3088fe4adcfa9081d2cc7a20c6d7cccfbdcbb
Berkay Ulguel <berkayyyulguel@gmail.com>
2021-05-07 15:07:21 +0300

    table component unit test v1

--- apps/coin-market/src/app/app.module.ts
+++ apps/coin-market/src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CurrencyPipe, DecimalPipe, registerLocaleData } from '@angular/common';
-import { MarketsService } from './shared/services/markets.service';
 import { AppRoutingModule } from './app-routing.module';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { tr_TR } from 'ng-zorro-antd/i18n';
 import tr from '@angular/common/locales/tr';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MarketsService } from './shared/services/markets.service';
 
 registerLocaleData(tr);
 @NgModule({
