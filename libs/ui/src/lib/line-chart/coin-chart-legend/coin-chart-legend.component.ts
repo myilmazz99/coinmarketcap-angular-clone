626722151f74e79f3b57bd441c77efd0d7c263cd
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2021-05-09 19:31:41 +0300

    implemented styles for chart legend checkboxes

--- libs/ui/src/lib/line-chart/coin-chart-legend/coin-chart-legend.component.ts
+++ libs/ui/src/lib/line-chart/coin-chart-legend/coin-chart-legend.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input } from '@angular/core';
+/* eslint-disable no-case-declarations */
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'coin-chart-legend',
     templateUrl: './coin-chart-legend.component.html',
     styleUrls: ['./coin-chart-legend.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class CoinChartLegendComponent {
     @Input() legend: string[] = [];
@@ -14,15 +16,19 @@ export class CoinChartLegendComponent {
 
         switch (name) {
             case 'BTC':
-                var serie_btc = this.chart.series.find((s) => s.name === name);
+                const serie_btc = this.chart.series.find(
+                    (s) => s.name === name
+                );
                 if (checked) serie_btc?.show();
                 else serie_btc?.hide();
                 break;
             case 'USD':
-                var serie_usd = this.chart.series.find((s) => s.name === name);
+                const serie_usd = this.chart.series.find(
+                    (s) => s.name === name
+                );
                 if (!checked) serie_usd.hide();
                 else serie_usd.show();
-
+                break;
             default:
                 break;
         }
