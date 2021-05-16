a83f62c5782d88b1a7cadb8ec8e16f77ca0f76e8
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2021-05-16 17:30:09 +0300

    added e2e test for vote-coin

---
+++ apps/coin-market-e2e/src/integration/vote-coin.spec.ts
@@ -0,0 +1,13 @@
+describe('vote-coin', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
+    it('should display you voted text', () => {
+        cy.get('coin-market-vote-coin').contains('How do you feel');
+
+        cy.get('.vote-coin__button-group__button').contains('Good').click();
+
+        cy.get('coin-market-vote-coin').should('contain', 'You voted');
+    });
+});
