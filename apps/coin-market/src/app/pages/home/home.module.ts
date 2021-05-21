125ca70204b160fa8afad03c993cd5c6055f7586
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2021-05-21 22:14:53 +0300

    added missing modules

--- apps/coin-market/src/app/pages/home/home.module.ts
+++ apps/coin-market/src/app/pages/home/home.module.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CoinListComponent } from '../../components/coin-list/coin-list.component';
+import { MaterialModule } from '../../shared/material.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
-    imports: [HomeRoutingModule],
+    imports: [HomeRoutingModule, CommonModule, MaterialModule],
     declarations: [HomeComponent, CoinListComponent],
     exports: [HomeComponent],
 })
