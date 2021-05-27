43f482fde3e394e09ba312586947e4a5777514e9
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2021-05-28 01:16:35 +0300

    deleted unnecessary e2e test

--- apps/coin-market-e2e/src/integration/app.spec.ts
+++ apps/coin-market-e2e/src/integration/app.spec.ts
@@ -1,18 +1,1 @@
-describe('coin-market', () => {
-    beforeEach(() => cy.visit('/home'));
 
-    it('should vote good for coin', () => {
-        cy.clearLocalStorage();
-        cy.get('.vote-coin__button-group__button')
-            .contains('Good')
-            .click()
-            .should(() => {
-                const token = localStorage.getItem('Bitcoin_vote');
-                expect(token).to.not.undefined;
-                expect(JSON.parse(token)).keys('value', 'expireDate');
-                expect(JSON.parse(token).value).eq(1);
-            });
-
-        cy.get('coin-market-vote-coin').contains('You voted');
-    });
-});
