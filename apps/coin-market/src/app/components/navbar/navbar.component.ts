e0af235285b15b8ac2b44b9e0b3f3908eeed5b77
myilmazz99 <46033929+myilmazz99@users.noreply.github.com>
2021-05-31 04:34:00 +0300

    responsive navbar v0.1
    Co-authored-by: Berkay Ulguel <BerkayyyU@users.noreply.github.com>

--- apps/coin-market/src/app/components/navbar/navbar.component.ts
+++ apps/coin-market/src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'coin-market-navbar',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+    @ViewChild('expandMenu') expandMenu: ElementRef;
+
+    hamburgerToggle() {
+        this.expandMenu.nativeElement.classList.toggle('expanded');
+
+        document.body.classList.toggle('overflow-hidden');
+    }
+}
